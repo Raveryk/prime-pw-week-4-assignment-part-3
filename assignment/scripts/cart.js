a5aa9edb913702cda1a94b730cad21d188b093a3
Reese Avery Kling <raveryk@gmail.com>
2021-03-19 16:25:55 -0500

    Worked on isFull function within addItem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,14 +4,25 @@ console.log('***** Cart Functions *****');
 //Question 1:
 
 let basket = [];
+const maxItems = 5; //STRETCH GOAL
 
 //Question 2:
 
 // function to add items into basket.
 function addItem( item ) {
-  basket.push(item); // each value of item should be entered into the basket array.
-  return true; // to check that the item was pushed into basket it will return true.
+  if (isFull( item ) === false) { //STRETCH GOAL//If there are 5 or less items in the basket then it will push the item in and return true.
+  basket.push(item);
+  return true;
+} else // If there are more than 5 items in the basket then I should see false returned.
+  return false;
 } // end addItem
+
+// function addItem( item ) { //Original REQUIRED function
+//   basket.push(item);
+//   return true;
+// } // end addItem
+
+
 
 console.log(`Basket is ${basket}`);
 console.log('Adding blue (expect true)', addItem('blue'));
@@ -20,12 +31,15 @@ console.log(`Basket is now ${basket}`);
 console.log('Adding red', addItem('red'));
 console.log('Adding green', addItem('green'));
 console.log('Adding purple', addItem('purple'));
+console.log('Adding yellow', addItem('yellow'));
+console.log('Adding grey', addItem('grey'));
+console.log('Adding orange', addItem('orange'));
 console.log(`Basket is now ${basket}`);
 
 //Question 3:
 
 //function to list items out.
-function listItems() {
+function listItems( basket ) {
   for (let i=0; i < basket.length; i++) { // this loop will iterate through the basket array.
     console.log(basket[i]); // I should see each index value of basket in the console.
   }
@@ -40,21 +54,26 @@ function listItems() {
 // }
 
 // testing function
-console.log( listItems() );
+listItems( basket );
 
 //Question 4:
 
 // function to empty basket
-function empty() {
+function empty() { //* When I pass an argumet through (basket) it logs the array as normal without emptying? But if I leave the arguments blank and then call the function afterward it shows an empty array. Why is that?
   basket = []; // Changing the value of basket to an empty array.
   if (basket.length === 0) { // Checking to see that if the array is actually empty, then true will be returned.
-    return true
+    return true;
+  } else {
+    return false;
   }
 }
 
-console.log('Is my basket empty?', empty()); // Testing empty()
+empty( basket );
+console.log('Is my basket empty?', empty() ); // Testing empty()
+console.log(basket);
 
-// * I again initially tried the above code but then after class decided to try and different method.
+
+// * I again initially tried the above code but then after class decided to try a different method.
 
 // function empty() {
 //   let index = 0;
@@ -69,11 +88,11 @@ console.log('Is my basket empty?', empty()); // Testing empty()
 // console.log('Is basket empty?', empty());
 
 
-//[[STRETCH]]
+//[[STRETCH GOALS]]
 
-//5.
 
-const maxItems = 5;
+
+
 
 function isFull() {
   if ( basket.length < maxItems ) {
